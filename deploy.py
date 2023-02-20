da79d19b59850ab2ed2a007f6789f485430ad493
Juan Pilorget <jpilorget@gmail.com>
2023-02-20 13:46:25 +0100

    Update credentials

--- deploy.py
+++ deploy.py
@@ -11,7 +11,7 @@ import botocore
 initial_instance_count = 1
 endpoint_instance_type = 'ml.m5.large'
 
-BUCKET_NAME = 'sample-sagemaker-cicd-tuto1'
+BUCKET_NAME = 'gh-actions-test-bucket'
 PREFIX = 'boston-housing-regression'
 OBJECT_KEY = f'{PREFIX}/reports.csv'
 
